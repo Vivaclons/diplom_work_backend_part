0bc9601eec61d40f69dc5d28f421988d1a06c068
Vivaclons <s.erkyn00@gmail.com>
2022-04-22 13:21:22 +0600

    branch for web part

--- appointmentService/src/main/java/kz/spring/appointmentservice/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package kz.spring.appointmentservice.repository;
-
-import kz.spring.appointmentservice.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer getCustomerByCustomerId(Long id);
-}
--- appointmentService/src/main/java/kz/spring/appointmentservice/repository/MedCenterRepository.java
+++
@@ -1,8 +0,0 @@
-package kz.spring.appointmentservice.repository;
-
-import kz.spring.appointmentservice.model.MedCenter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedCenterRepository extends JpaRepository<MedCenter, Long> {
-    MedCenter getMedCenterByMedCenterId(Long id);
-}
