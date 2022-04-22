b73b80a1a8062742e859874c6360827fddb24191
Vivaclons <s.erkyn00@gmail.com>
2022-04-22 13:22:58 +0600

    new branch for mobile part

--- appointmentService/src/main/java/kz/spring/appointmentservice/model/AppointmentStatus.java
+++
@@ -1,7 +0,0 @@
-package kz.spring.appointmentservice.model;
-
-public enum AppointmentStatus {
-    WAITING_TO_ARRIVAL,
-    PATIENT_DID_NOT_ARRIVE,
-    APPOINTMENT_ACCEPTED;
-}
--- appointmentService/src/main/java/kz/spring/appointmentservice/repository/DoctorRepository.java
+++
@@ -1,8 +0,0 @@
-package kz.spring.appointmentservice.repository;
-
-import kz.spring.appointmentservice.model.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-    Doctor getDoctorByDoctorId(Long id);
-}
